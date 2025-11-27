a353179a5a59b313a129058033cb41a0ec0e8518
Вадим Бажов <master@remort.net>
2025-11-27 18:13:45 +0700

    BT-4017: Fork synced and parser.py updated.

--- parser.py
+++ parser.py
@@ -7,6 +7,11 @@ CHAINS_PATH = BASE_PATH / 'blockchains'
 EXPORT_DIR_PATH = BASE_PATH / 'export data'
 
 CHAINS = ['ethereum', 'polygon', 'tron']
+CHAINS_MAP = {
+    'ethereum': 'ETH',
+    'polygon': 'POLYGON',
+    'tron': 'TRON',
+}
 
 TRON_WALLET_FORMAT_RE = r'^(0x41[a-fA-F0-9]{40})|(T[a-km-zA-HJ-NP-Z1-9]{33})$'
 ETH_WALLET_FORMAT_RE = r'^0x[a-fA-F0-9]{40}$'
@@ -16,9 +21,13 @@ ADDRESS_VALIDATORS = {
     'tron': TRON_WALLET_FORMAT_RE,
 }
 
+result = {
+    'meta': {'source_slug': 'trustwallet-assets'},
+    'data': {},
+}
+
 def run():
     for chain in CHAINS:
-        results = {}
         addrs_dir = CHAINS_PATH / chain / 'assets'
         for p in Path(addrs_dir).iterdir():
             if p.is_dir():
@@ -30,19 +39,17 @@ def run():
                         data = json.loads(raw_data)
                     except json.decoder.JSONDecodeError as err:
                         print(p.name, err, '\nFile content was:\n', raw_data)
-                results[data['id']] = {
+                result['data'][data['id']] = {
                     'name': data['name'],
                     'symbol': data['symbol'],
                     'website': data['website'] or None,
                     'status': data['status'],
+                    'is_contract': True,
+                    'chains': [CHAINS_MAP[chain]],
                 }
 
-        print(chain, len(results))
-
-        EXPORT_DIR_PATH.mkdir(parents=True, exist_ok=True)
-        with open(EXPORT_DIR_PATH / f'{chain.upper()}_results.json', 'w') as f:
-            f.write(json.dumps(results, indent=4))
-
+    with open('dataset.json', 'w') as f:
+        f.write(json.dumps(result, indent=2))
 
 if __name__ == '__main__':
     run()
